59444126a393ccf357e329f75a79f559ae658d3e
kmyourim <dbfla704@gmail.com>
2025-02-17 03:40:54 +0900

    design(css): UI 변경

--- app/components/ItemCard.js
+++ app/components/ItemCard.js
@@ -7,8 +7,8 @@ export default function ItemCard({ item }) {
       href={`/community/${item._id}`}
       className="block"
     >
-      <div className="flex items-center p-4 rounded-lg shadow hover:shadow-lg transition-shadow">
-        <div className="w-56 h-56 bg-gray-200 flex items-center justify-center rounded-lg">
+      <div className="flex items-center p-8 rounded-lg shadow hover:shadow-lg transition-shadow">
+        <div className="w-48 h-48 bg-gray-200 flex items-center justify-center rounded-lg">
           {item.imageUrls && item.imageUrls.length > 0 ? (
             <img 
               src={item.imageUrls[0]} 
@@ -19,22 +19,24 @@ export default function ItemCard({ item }) {
             <span className="text-gray-500">이미지 없음</span>
           )}
         </div>
-        <div className="ml-6 flex-1">
-          <div className="flex items-center justify-between mb-2">
-            {item.recommended ? (
-              <span className="text-blue-600 font-bold">추천템</span>
-            ) : (
-              <span className="text-red-600 font-bold">비추천템</span>
-            )}
-            <span className="text-sm text-gray-500">
+        <div className="ml-12 flex-1">
+          <div className="flex items-center justify-between mb-5">
+            <div className="flex items-center gap-2">   
+              {item.recommended ? (
+                <span className="px-3 py-1 bg-blue-100 rounded-full text-blue-600 font-bold">추천템</span>
+              ) : (
+                <span className="px-3 py-1 bg-pink-100 rounded-full text-red-400 font-bold">비추천템</span>
+              )}
+              <p className="font-semibold text-2xl whitespace-pre-wrap">{item.name}</p>
+            </div>
+            <span className="text-base text-gray-500">
               {new Date(item.createdAt).toLocaleDateString('ko-KR')}
             </span>
           </div>
-          <p className="font-semibold text-xl">{item.name}</p>
-          <p className="text-l text-gray-600">{item.description}</p>
-          <p className="text-l text-gray-600">사용 연령: {item.ageGroup}</p>
-          <p className="text-l text-gray-600">
-            구매처: {item.purchaseLink || '미기재'}
+          <p className="text-l text-gray-700 whitespace-pre-wrap mb-1">내용              {item.description}</p>
+          <p className="text-l text-gray-700 whitespace-pre-wrap mb-1">사용 연령      {item.ageGroup}</p>
+          <p className="text-l text-gray-700 whitespace-pre-wrap mb-1">
+            구매처           {item.purchaseLink || '미기재'}
           </p>
         </div>
       </div>
